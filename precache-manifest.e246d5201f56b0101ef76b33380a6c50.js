a3c3a3c527864484114322d43f16445b511dc714
moises <moises.huaringa>
2022-12-20 16:38:29 -0500

    Updates

---
+++ precache-manifest.e246d5201f56b0101ef76b33380a6c50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930c231a996464d465925060a7a6f891",
+    "url": "/dummy-logo-maker-v1/index.html"
+  },
+  {
+    "revision": "3cb779db035aa02ed133",
+    "url": "/dummy-logo-maker-v1/static/js/2.b98713b4.chunk.js"
+  },
+  {
+    "revision": "fde5ff8f4122a466397a13ef98d4f94d",
+    "url": "/dummy-logo-maker-v1/static/js/2.b98713b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a76b1bec81393fa8779",
+    "url": "/dummy-logo-maker-v1/static/js/main.df4fc513.chunk.js"
+  },
+  {
+    "revision": "fbe495a42e9663d3d519",
+    "url": "/dummy-logo-maker-v1/static/js/runtime-main.2135b0c5.js"
+  }
+]);
